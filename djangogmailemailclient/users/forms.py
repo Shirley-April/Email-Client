a435c7598de8cae080a9c4bd43588b282962467c
Shirley-April <shirleyapril23@gmail.com>
2021-08-19 18:23:25 +0300

    adding a view to signup a user

--- djangogmailemailclient/users/forms.py
+++ djangogmailemailclient/users/forms.py
@@ -8,4 +8,38 @@ class UserSignUpForm(UserCreactionForm):
     email = forms.EmailField
 
     class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+class EmailMessageForm(forms.Form):
+    #get all active registered users
+
+
+    CHOICES = User.objects.all().values_list(
+        #the data to be held in the value field of the dropdowns e.g. the HTML option tag 'id'
+
+        Concat(
+            F('first_name'), V(' '), F('last_name'),
+
+            #how the data is to be displayed
+            output_field=CharField()
+        )   
+    )
+    # the drop-down to hold the registered active users (users not deleted/deactivated) fullnames and user database ids
+    # the drop-down is to be populated with data from the choices variable above
+
+    user_id = forms.ChoiceField(choices=CHOICES)
+
+    # the email subject
+
+    subject = forms.CharField(max_length=200)
+
+    # the email message textarea
+
+    message = forms.CharField(max_length=2000, widget=forms.Textarea)
+
+    def __init__(self, *args, **kwargs):
+        super(EmailMessageForm, self).__init__(*args, **kwargs) # Call to  ModelForm constructor
+        self.fields['subject'].widget.attrs['style'] = 'width:100%'
+        self.fields['message'].widget.attrs['cols'] = 150
         
--- djangogmailemailclient/users/views.py
+++ djangogmailemailclient/users/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib import messages 
+from users.forms import UserSignUpForm, EmailMessageForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'users/index.html')
+
+def signup(request):
+
+    if request.method == 'POST':
+        # instantiate  a new form with the post data submited
+        form = UserSignUpForm(request.POST)
+
+        if form.is_valid():
+            # call the form.save() method to save the data to the DB
+            form.save()
+            messages.success(request, f'Your account has been created')
+            return redirect('login')
+    # pass a blank sign up form when a user navigates to this page through a GET request
+    else:
+        form = UserSignUpForm()
+    return render(request, 'users/signup.html')
+
+
+@login_reqired
+def send_email(request):
+    if request.method == "POST":
+        form = EmailMessageForm(request.POST)
+        if form.is_valid()
+        #get the submitted subject
+
+        subject = form.cleaned_data['subject']
+
+        # get the submitted message
+
+        message = form.cleaned_data['message']
+
+        # get the submitted user_id which is the value of the option that was selected and submitted
+        user_id = form.cleaned_data['user_id']
+
+        # get the user associated with the passed used_id
+        user = User.objects.get(user_id)
+
+
+        user.email_user(subject, message, request.user.get_full_name(), fail_silently=True)
+
+        messages.success(request, f'An email has been sent to {user.get_full_name()}')
+
+        return redirect('index')
+
+    # pass the blan email message form when a user navigates to the email page through a GET request
+
+    else:
+        form = EmailMessageForm()
+
+        context = {'form': form}
+
+        return render(request, 'users/send_email.html')
